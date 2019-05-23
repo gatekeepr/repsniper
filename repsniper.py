70fb3a08001f369b21818cc902446b94a200488f
Henrik Holm <henrikholm@gmx.de>
2019-05-23 12:47:24 +0200

    exceptions are now being caught

--- repsniper.py
+++ repsniper.py
@@ -31,30 +31,13 @@ while 1:
             recent = submission.title
             url = submission.url
 
-<<<<<<< HEAD
-        # check if new post available and reset variables
-        if latest not in recent:
-            for submission in subreddit.new(limit=1):
-                recent = submission.title
-                url = submission.url
-            latest = recent
-            trigger = False
-
         # call message fn if its an european listing and hasnt been sent yet
-        if trigger is not True and "US" not in recent and "CAN" not in recent and "AUS" not in recent:
+        check = recent.upper()
+        if trigger is not True and "[US]" not in check and "[USA]" not in check and "[CAN]" not in check and "[AUS]" not in check and "[ASIA]" not in check:
             trigger = True
             send_telegram_message(recent, url)
-
-    except praw.requests.exceptions.HTTPError as e:
+    except HTTPError as e:
         msg = "HTTPError(" + str(e.errno) + "): " + str(e.strerror)
         print(msg)
         pass
-        
-=======
-    # call message fn if its an european listing and hasnt been sent yet
-    check = recent.upper()
-    if trigger is not True and "[US]" not in check and "[USA]" not in check and "[CAN]" not in check and "[AUS]" not in check and "[ASIA]" not in check:
-        trigger = True
-        send_telegram_message(recent, url)
->>>>>>> 3ac99e3d0aa95f19a7005ab9bca44c7bc4d5036c
     time.sleep(10)
