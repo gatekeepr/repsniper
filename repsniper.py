cc251c2cdfa0be9a0c9db183a67080f2ba11db84
Henrik Holm <henrikholm@gmx.de>
2019-05-24 23:27:47 +0200

    fixed exceptions for real now

--- repsniper.py
+++ repsniper.py
@@ -6,6 +6,7 @@ import os
 import time
 import telegram
 import configparser
+import urllib2
 
 # load configurations
 config = configparser.ConfigParser()
@@ -25,19 +26,34 @@ def send_telegram_message(title, url):
 
 # main loop that checks for new posts containing our keywords
 while 1:
-    # get recent submission
     try:
+        # get recent submission
         for submission in subreddit.new(limit=1):
             recent = submission.title
             url = submission.url
+        # check if new post available and reset variables
+        if latest not in recent:
+            for submission in subreddit.new(limit=1):
+                recent = submission.title
+                url = submission.url
+            latest = recent
+            trigger = False
 
         # call message fn if its an european listing and hasnt been sent yet
         check = recent.upper()
         if trigger is not True and "[US]" not in check and "[USA]" not in check and "[CAN]" not in check and "[AUS]" not in check and "[ASIA]" not in check:
             trigger = True
             send_telegram_message(recent, url)
-    except HTTPError as e:
-        msg = "HTTPError(" + str(e.errno) + "): " + str(e.strerror)
-        print(msg)
-        pass
+    # catch exceptions when reddit is down
+    except urllib2.HTTPError, e:
+        if e.code in [429, 500, 502, 503, 504]:
+            print "Reddit is down (error %s), sleeping..." % e.code
+            time.sleep(60)
+            pass
+        else:
+            raise
+    # catch every other exception
+    except Exception, e:
+        print "Not a HTTP Exception, ERROR: %s" % str(e)
+        raise
     time.sleep(10)
